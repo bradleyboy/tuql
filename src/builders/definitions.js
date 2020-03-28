9fb9866b92052338a6f71b7d5ff01637709de788
Renato Atilio <renatoat@gmail.com>
2020-03-27 21:31:34 -0300

    Fix autoIncrement definiton (column.type is lowercase) which was causing create mutations to return id: null

--- src/builders/definitions.js
+++ src/builders/definitions.js
@@ -40,7 +40,7 @@ export default columns => {
       field: column.name,
       allowNull: column.notnull === 0 || column.dflt_value !== null,
       defaultValue: column.dflt_value,
-      autoIncrement: column.type === 'INTEGER' && column.pk === 1,
+      autoIncrement: column.type === 'integer' && column.pk === 1,
     };
 
     return acc;
